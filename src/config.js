78350c3f068236d6cbacd8d3bf21c4679d243c99
trwiley <taylor.r.wiley@gmail.com>
2022-09-12 14:56:58 -0400

    delete EVERYTHING

--- src/config.js
+++
@@ -1,3 +0,0 @@
-export const GAME_NAME = "Coup";
-export const DEFAULT_PORT = 8000;
-export const APP_PRODUCTION = process.env.NODE_ENV === "production";
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Home from "./Home/Home";
-import Room from "./Room/Room";
-
-export { Home, Room };
