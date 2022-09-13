545cb14f747a1bfc9b07e8d82a37d9c5dae6a7f3
Rob Wieck <rwieck@Robs-MacBook-Pro-4.local>
2022-09-13 14:12:26 -0500

    TE-30 adding to gitignore

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const PRODUCTION_EVIRONMENT = "production"; //else development
-export const BASE_URL = "https://thirsty-earth.crc.nd.edu"; //need to put in port for multiplayer (none for prod)
+export const BASE_URL = "localhost:8000"; //need to put in port for multiplayer (none for prod)
 export const DEFAULT_PORT = 8000;
