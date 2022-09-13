f4dc40f46a354460c5cf0cb5c88f7907331fbf87
trwiley <taylor.r.wiley@gmail.com>
2022-09-13 13:54:04 -0400

    Add screen to enter name and link to game

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { GameHub } from './GameHub';
+import { EnterName } from "./EnterName";
 
 
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<GameHub/>} />
+      <Route path="/game" element={<GameHub/>} />
+      <Route path="/" element={<EnterName/>} />
     </Routes>
   </BrowserRouter>
 
---
+++ src/EnterName.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const divStyle = {
+    textAlign: 'center',
+    marginTop: '15%'
+}
+
+const extraButtonStyle = {
+    marginTop: '18px'
+}
+
+const inputStyle = { 
+    fontSize: '18px',
+    borderRadius: '15px'
+}
+export function EnterName() {
+    return(
+        <div style={divStyle}>
+            <label for="name"><h2>Your Name</h2></label>
+            <input type="text" id="name" required style={inputStyle}></input><br />
+            <Link to='/game'>
+                <button className='button-style' style={extraButtonStyle}>Enter Game</button>
+            </Link>
+           
+        </div>
+    )
+}
