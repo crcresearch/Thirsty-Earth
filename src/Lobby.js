90c217808f109a8b9610dcc99ad9a95ec3f909ce
trwiley <taylor.r.wiley@gmail.com>
2022-09-21 16:54:59 -0400

    WIP: it's in a working state again??

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { LobbyClient } from 'boardgame.io/client';
+//import { useRecoilState } from "recoil";
+
+// import { gameIDAtom } from "./atoms/gameid";
+// import { playerIDAtom } from "./atoms/pid";
+// import { playerCredentialsAtom } from "./atoms/playercred";
 
 const divStyle = {
     textAlign: 'center',
@@ -16,7 +22,42 @@ const inputStyle = {
     marginBottom: '18px'
 }
 
+
+
+const lobbyClient = new LobbyClient({ server: 'http://localhost:8080' });
+
+
+
 export function EnterName() {
+    //const [gameID, setGameID] = useRecoilState(gameIDAtom);
+    //const [playerID, setPlayerID] = useRecoilState(playerIDAtom);
+    //const [playerCredentials, setPlayerCredentials] = useRecoilState(playerCredentialsAtom);
+
+    function createMatch() {
+        lobbyClient.createMatch('push-the-button', {
+            numPlayers: 2
+        })
+        .then(({ matchID }) => {
+            lobbyClient.joinMatch( 'push-the-button', 
+            matchID,
+            {
+                playerName: 'bob'
+            })
+        });
+
+        //setGameID(matchID);
+    
+        // const {newPlayerCredentials, newPlayerID } = await lobbyClient.joinMatch(
+        //     'push-the-button', 
+        //     matchID,
+        //     {
+        //         playerName: 'bob'
+        //     })
+        
+        // setPlayerCredentials(newPlayerCredentials);
+        // setPlayerID(newPlayerID);
+    }
+
     return(
         <div style={divStyle}>
             <h2>Enter Game</h2>
@@ -24,9 +65,9 @@ export function EnterName() {
             <input type="text" id="roomid" required style={inputStyle}></input><br />
             <label htmlFor="name">Your name: </label>
             <input type="text" id="name" required style={inputStyle}></input><br />
-            <Link to='/game'>
-                <button className='button-style' style={extraButtonStyle}>Join</button>
-            </Link>
+        
+            <button className='button-style' style={extraButtonStyle} onClick={createMatch()}>Join</button>
+       
 
             <h2>Create Game</h2>
             <label htmlFor="name">Your name: </label>
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const { Server, Origins } =  require('boardgame.io/server');
 const { PushTheButtonFrank } = require('./Game');
+import cors from "@koa/cors";
+
+const lobbyConfig = {
+    apiPort: 8080,
+    apiCallback: () => console.log('Running Lobby API on port 8080...'),
+  };
 
 const server = Server({
     games: [PushTheButtonFrank],
     origins: [Origins.LOCALHOST],
 });
 
-server.run(8000);
+server.run({port: 8000, lobbyConfig});
+server.app.use(cors({ origin: "*" }));
