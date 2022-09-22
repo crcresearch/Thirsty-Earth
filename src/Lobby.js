18b572b8297d7c4199d3295ae0d34df2e7d35083
trwiley <taylor.r.wiley@gmail.com>
2022-09-22 11:16:07 -0400

    Flesh out create/join match, add atoms back

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LobbyClient } from 'boardgame.io/client';
-//import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 
-// import { gameIDAtom } from "./atoms/gameid";
-// import { playerIDAtom } from "./atoms/pid";
-// import { playerCredentialsAtom } from "./atoms/playercred";
+import { gameIDAtom } from "./atoms/gameid";
+import { playerIDAtom } from "./atoms/pid";
+import { playerCredentialsAtom } from "./atoms/playercred";
 
 const divStyle = {
     textAlign: 'center',
@@ -29,9 +30,10 @@ const lobbyClient = new LobbyClient({ server: 'http://localhost:8080' });
 
 
 export function EnterName() {
-    //const [gameID, setGameID] = useRecoilState(gameIDAtom);
-    //const [playerID, setPlayerID] = useRecoilState(playerIDAtom);
-    //const [playerCredentials, setPlayerCredentials] = useRecoilState(playerCredentialsAtom);
+    const setGameID = useSetRecoilState(gameIDAtom);
+    const setPlayerID = useSetRecoilState(playerIDAtom);
+    const setPlayerCredentials = useSetRecoilState(playerCredentialsAtom);
+    const [matchIDQuery, setMatchIDQuery] = useState("");
 
     function createMatch() {
         lobbyClient.createMatch('push-the-button', {
@@ -43,37 +45,56 @@ export function EnterName() {
             {
                 playerName: 'bob'
             })
+            .then(({ pid, playerCreds }) => {
+                setPlayerID(pid);
+                setPlayerCredentials(playerCreds);
+            });
+            setGameID(matchID);
         });
+    }
 
-        //setGameID(matchID);
-    
-        // const {newPlayerCredentials, newPlayerID } = await lobbyClient.joinMatch(
-        //     'push-the-button', 
-        //     matchID,
-        //     {
-        //         playerName: 'bob'
-        //     })
-        
-        // setPlayerCredentials(newPlayerCredentials);
-        // setPlayerID(newPlayerID);
+    function joinMatch(matchID) {
+        setGameID(matchID);
+        lobbyClient.joinMatch(
+            'push-the-button',
+            matchID,
+            {
+                playerName: 'alice'
+            }
+        )
+        .then(({ pid, playerCreds}) => {
+            setPlayerID(pid);
+            setPlayerCredentials(playerCreds);
+        });
     }
 
     return(
         <div style={divStyle}>
             <h2>Enter Game</h2>
             <label htmlFor="roomid">Room ID: </label>
-            <input type="text" id="roomid" required style={inputStyle}></input><br />
+            <input 
+                type="text" 
+                id="roomid" 
+                required 
+                style={inputStyle}
+                value={matchIDQuery}
+                onChange={(event) => {
+                    setMatchIDQuery(event.target.value);
+                }}
+            >
+            </input>
+            <br />
             <label htmlFor="name">Your name: </label>
             <input type="text" id="name" required style={inputStyle}></input><br />
         
-            <button className='button-style' style={extraButtonStyle} onClick={createMatch()}>Join</button>
+            <button className='button-style' style={extraButtonStyle} onClick={() => joinMatch(matchIDQuery)}>Join</button>
        
 
             <h2>Create Game</h2>
             <label htmlFor="name">Your name: </label>
             <input type="text" id="name" required style={inputStyle}></input><br />
             <Link to='/game'>
-                <button className='button-style' style={extraButtonStyle}>Create</button>
+                <button className='button-style' style={extraButtonStyle} onClick={createMatch}>Create</button>
             </Link>  
         </div>
     )
