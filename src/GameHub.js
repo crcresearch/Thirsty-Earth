c320d54871f1142ee8a2d9cbc81176889610f450
trwiley <taylor.r.wiley@gmail.com>
2022-09-26 11:23:10 -0400

    WIP: troubleshooting lobby

--- src/GameHub.js
+++ src/GameHub.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useRecoilValue } from 'recoil';
 
 import { SocketIO } from 'boardgame.io/multiplayer';
 import { Client } from 'boardgame.io/react';
@@ -6,19 +7,33 @@ import { Client } from 'boardgame.io/react';
 import { PushTheButtonFrank} from "./Game";
 import { ButtonBoard } from './Board';
 
+import { gameIDAtom } from "./atoms/gameid";
+import { playerIDAtom } from "./atoms/pid";
+import { playerCredentialsAtom } from "./atoms/playercred";
+
 
 
 export function GameHub() {
+
     const PushTheButtonClient = Client({
         game: PushTheButtonFrank,
         board: ButtonBoard,
         multiplayer: SocketIO({ server: 'localhost:8000' }),
     });
 
+    const gameID = useRecoilValue(gameIDAtom);
+    const playerID = useRecoilValue(playerIDAtom);
+    const playerCredentials = useRecoilValue(playerCredentialsAtom);
+
+    console.log('player ID: ', playerID);
+
 
     return(
         <div>
-            <PushTheButtonClient playerID='0' />
+            <PushTheButtonClient 
+                playerID={playerID}
+                credentials={playerCredentials}
+                matchID={gameID} />
         </div>
     )
 }
