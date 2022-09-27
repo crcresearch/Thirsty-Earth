80e85efedf96feadcc07f31c00cf7650213f25a4
Caleb Reinking <creinkin@or-r634hd4j2y.dhcp.nd.edu>
2022-09-27 13:30:33 -0400

    adding bootstrap, bringing in a few of Joe's theme choices

--- src/Lobby.js
+++ src/Lobby.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { LobbyClient } from 'boardgame.io/client';
+import { LobbyClient } from "boardgame.io/client";
 import { useSetRecoilState } from "recoil";
 
 import { gameIDAtom } from "./atoms/gameid";
@@ -9,95 +9,127 @@ import { playerIDAtom } from "./atoms/pid";
 import { playerCredentialsAtom } from "./atoms/playercred";
 
 const divStyle = {
-    textAlign: 'center',
-    marginTop: '5%'
-}
+  textAlign: "center",
+  marginTop: "5%",
+};
 
 const extraButtonStyle = {
-    marginTop: '18px'
-}
+  marginTop: "18px",
+};
 
-const inputStyle = { 
-    fontSize: '18px',
-    borderRadius: '15px',
-    marginBottom: '18px'
-}
+const inputStyle = {
+  fontSize: "18px",
+  borderRadius: "15px",
+};
 
-
-
-const lobbyClient = new LobbyClient({ server: 'http://localhost:8080' });
-
-
+const lobbyClient = new LobbyClient({ server: "http://localhost:8080" });
 
 export function EnterName() {
-    const setGameID = useSetRecoilState(gameIDAtom);
-    const setPlayerID = useSetRecoilState(playerIDAtom);
-    const setPlayerCredentials = useSetRecoilState(playerCredentialsAtom);
-    const [matchIDQuery, setMatchIDQuery] = useState("");
-    let navigate = useNavigate();
+  const setGameID = useSetRecoilState(gameIDAtom);
+  const setPlayerID = useSetRecoilState(playerIDAtom);
+  const setPlayerCredentials = useSetRecoilState(playerCredentialsAtom);
+  const [matchIDQuery, setMatchIDQuery] = useState("");
+  let navigate = useNavigate();
 
-    function createMatch() {
-        lobbyClient.createMatch('push-the-button', {
-            numPlayers: 2
-        })
-        .then(({ matchID }) => {
-            lobbyClient.joinMatch( 'push-the-button', 
-            matchID,
-            {
-                playerName: 'bob'
-            })
-            .then((playerInfo) => {
-                console.log(playerInfo);
-                setPlayerID(playerInfo.playerID);
-                setPlayerCredentials(playerInfo.playerCredentials);
-                
-                navigate("/game", { replace: true} );
-            });
-            setGameID(matchID);
-            console.log(matchID);
-        });
-    }
-
-    function joinMatch(matchID) {
-        setGameID(matchID);
-        lobbyClient.joinMatch(
-            'push-the-button',
-            matchID,
-            {
-                playerName: 'alice'
-            }
-        )
-        .then((playerInfo) => {
+  function createMatch() {
+    lobbyClient
+      .createMatch("push-the-button", {
+        numPlayers: 2,
+      })
+      .then(({ matchID }) => {
+        lobbyClient
+          .joinMatch("push-the-button", matchID, {
+            playerName: "bob",
+          })
+          .then((playerInfo) => {
+            console.log(playerInfo);
             setPlayerID(playerInfo.playerID);
             setPlayerCredentials(playerInfo.playerCredentials);
-            navigate("/game", { replace: true} );
-        });
-    }
 
-    return(
-        <div style={divStyle}>
-            <h2>Enter Game</h2>
-            <label htmlFor="roomid">Room ID: </label>
-            <input 
-                type="text" 
-                id="roomid" 
-                required 
-                style={inputStyle}
-                value={matchIDQuery}
-                onChange={(event) => {
-                    setMatchIDQuery(event.target.value);
-                }}
+            navigate("/game", { replace: true });
+          });
+        setGameID(matchID);
+        console.log(matchID);
+      });
+  }
+
+  function joinMatch(matchID) {
+    setGameID(matchID);
+    lobbyClient
+      .joinMatch("push-the-button", matchID, {
+        playerName: "alice",
+      })
+      .then((playerInfo) => {
+        setPlayerID(playerInfo.playerID);
+        setPlayerCredentials(playerInfo.playerCredentials);
+        navigate("/game", { replace: true });
+      });
+  }
+
+  return (
+    <div className="container mt-4">
+      <h2 className="title-font text-center mb-4">Thirsty Earth Lobby</h2>
+      <div className="row">
+        <div className="col-12 col-lg-4 offset-lg-2">
+          <h2 className="subtitle-font text-center">Join Game</h2>
+          <div className="mb-3">
+            {/* <label htmlFor="roomid"  className="form-label">Room ID: </label> */}
+            <input
+              type="text"
+              className="form-control"
+              id="roomid"
+              aria-describedby="roomid-input"
+              required
+              style={inputStyle}
+              value={matchIDQuery}
+              onChange={(event) => {
+                setMatchIDQuery(event.target.value);
+              }}
+              placeholder="Room ID"
+            ></input>
+          </div>
+          <input
+            type="text"
+            id="name"
+            placeholder="Your name"
+            required
+            className="form-control"
+            style={inputStyle}
+          ></input>
+
+          <div className="d-flex flex-row-reverse ">
+            <button
+              type="button"
+              className="btn btn-block btn-primary"
+              style={extraButtonStyle}
+              onClick={() => joinMatch(matchIDQuery)}
             >
-            </input>
-            <br />
-            <label htmlFor="name">Your name: </label>
-            <input type="text" id="name" required style={inputStyle}></input><br />
-            <button className='button-style' style={extraButtonStyle} onClick={() => joinMatch(matchIDQuery)}>Join</button>
-            
-            <h2>Create Game</h2>
-            <label htmlFor="name">Your name: </label>
-            <input type="text" id="name" required style={inputStyle}></input><br />
-            <button className='button-style' style={extraButtonStyle} onClick={createMatch}>Create</button>
+              Join
+            </button>
+          </div>
         </div>
-    )
+        <div className="col-12 col-lg-4">
+          <h2 className="subtitle-font text-center">Create Game</h2>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            placeholder="Your Name"
+            required
+            style={inputStyle}
+          ></input>
+          <div className="d-flex flex-row-reverse ">
+            <button
+              type="button"
+              className="btn btn-primary"
+              style={extraButtonStyle}
+              onClick={createMatch}
+            >
+              Create Game
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
