b444168eb716efd991a41a34eaaf8097944aa001
trwiley <taylor.r.wiley@gmail.com>
2022-10-04 13:57:25 -0400

    WIP: game board layout

--- src/Board.js
+++ src/Board.js
@@ -18,11 +18,45 @@ export function ButtonBoard({ ctx, G, moves }) {
         textAlign: 'center'
     }
 
+    const chatStyle = {
+        backgroundColor: 'red',
+        height: '100vh'
+    }
+
+    const gameBoardStyle = {
+        backgroundColor: 'blue',
+        height: '100vh'
+    }
+
+    const recordsStyle = {
+        backgroundColor: 'green',
+        height: '100vh'
+    }
+
+    // return (
+    //     <div style={divStyle}>    
+    //         <h1>Current player: {ctx.currentPlayer}</h1>
+    //         <h1>Button value: {G.buttonValue}</h1> 
+    //         <button style={buttonStyle} onClick={() => onClick()}> push me.</button> 
+    //     </div>
+    // )
     return (
-        <div style={divStyle}>    
-            <h1>Current player: {ctx.currentPlayer}</h1>
-            <h1>Button value: {G.buttonValue}</h1> 
-            <button style={buttonStyle} onClick={() => onClick()}> push me.</button> 
+        <div className="container">
+            <div className="row">
+                <h1>Year: 1</h1>
+                <h1>Village: Test</h1>
+            </div>
+            <div className="row">
+                <div className="col" style={chatStyle}>
+                    placeholder
+                </div>
+                <div className="col-6" style={gameBoardStyle}>
+                    placeholder
+                </div>
+                <div className="col" style={recordsStyle}>
+                    placeholder
+                </div>
+            </div>
         </div>
     )
 }
