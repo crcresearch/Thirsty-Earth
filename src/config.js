0286e508d215824289d5656530293e3a28e47e10
Rob Wieck <rwieck@Robs-MacBook-Pro-4.local>
2022-10-24 10:15:28 -0500

    trying to clean up gitignore

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCTION_EVIRONMENT = "production"; //else development
-export const BASE_URL = "http://localhost:8000"; //need to put in port for multiplayer (none for prod)
-export const API_URL = "http://localhost:8080";
-export const DEFAULT_PORT = 8000;
-export const API_PORT = 8080;
