f155e19486171dc5e34d6f86ba8bfa31c3e289c0
Rob Wieck <rwieck@Robs-MacBook-Pro-4.local>
2022-10-24 08:28:36 -0500

    changing default file to localhost

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PRODUCTION_EVIRONMENT = "production"; //else development
-export const BASE_URL = "https://thirsty-earth.crc.nd.edu"; //need to put in port for multiplayer (none for prod)
-export const API_URL = "https://thirsty-earth.crc.nd.edu";
+export const BASE_URL = "http://localhost:8000"; //need to put in port for multiplayer (none for prod)
+export const API_URL = "http://localhost:8080";
 export const DEFAULT_PORT = 8000;
-export const API_PORT = 5000;
+export const API_PORT = 8080;
