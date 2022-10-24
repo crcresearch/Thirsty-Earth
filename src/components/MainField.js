73c006546e37f7f0a829867e4f84bd3460626e44
trwiley <taylor.r.wiley@gmail.com>
2022-10-24 10:54:21 -0400

    WIP selecting tiles

--- src/components/MainField.js
+++ src/components/MainField.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
 import leaf from '../img/leaf.png';
 import briefcase from '../img/briefcase.png';
 import cloud from '../img/cloud.png';
@@ -7,6 +9,13 @@ import tumbleweed from '../img/tumbleweed.png';
 import well from '../img/well.png';
 import grass from "../img/grass.png"
 
+const options = [
+    cloud,
+    river,
+    well,
+    briefcase,
+    tumbleweed
+]
 const gameBoardStyle = {
     backgroundColor: '#31a61e',
     height: '800px',
@@ -19,12 +28,8 @@ const selectionsStyle = {
     marginTop: '30%'
 }
 
-const imgStyle = {
-    width: '100px',
-    height: '100px'
-}
 
- const cropGridStyle = {
+const cropGridStyle = {
     display: 'flex',
     flexDirection: 'row',
     marginLeft: '5%',
@@ -35,58 +40,101 @@ const cropSquareStyle = {
     borderStyle: 'solid'
 }
 
-export function MainField() {
+const imgStyle = {
+    width: '100px',
+    height: '100px',
+}
+
+const GameTile = ({
+    theKey,
+    isHighlighted,
+    image
+}) => {
     return(
+        <div style={{
+            ...cropSquareStyle,
+            border: isHighlighted 
+                ? 'solid cyan 1px' 
+                : undefined
+            }} 
+            key={theKey}>
+            <img src={image} style={imgStyle}/>
+        </div>
+    )
+}
+
+const SelectAction = ({
+    isHighlighted,
+    image,
+    altText
+}) => {
+    return(<img src={image} 
+            alt={altText} 
+            style={{
+                ...imgStyle,
+                border: isHighlighted 
+                ? 'solid cyan 1px' 
+                : undefined
+            }} />)
+}
+
+export function MainField() {
+    const [gameGrid, setGameGrid] = useState([['', '', ''], ['', '', ''], ['', '', '']]);
+    const [selectedOption, setSelectedOption] = useState('');
+
+    useEffect(() => {
+        let tempArray = gameGrid;
+        for (let i = 0; i < tempArray.length; i++) {
+            for (let j = 0; j < tempArray[i].length; j++) {
+                let cellObject = {
+                    image: leaf
+                };
+                tempArray[i][j] = { ...cellObject };
+            }
+        }
+        setGameGrid(tempArray);
+    }, [gameGrid]);
+
+
+
+    const selectSquare = ((row, col) => {
+        let tempArray = gameGrid;
+        tempArray[row][col].image = cloud;
+        setGameGrid(tempArray);
+        console.log(gameGrid);
+    });
+
+    const selectOption = ((option) => {
+        setSelectedOption(option);
+        console.log(selectedOption)
+    })
+
+
+
+    return (
         <div className="container thick-border" style={gameBoardStyle}>
             <div className="row">
                 <div className="col">
                     <div style={selectionsStyle}>
-                        <img src={cloud} alt="use rainwater" style={imgStyle}/>
-                        <img src={river} alt="use river water" style={imgStyle}/>
-                        <img src={well} alt="use well water" style={imgStyle}/>
-                        <img src={briefcase} alt="outside employment" style={imgStyle}/>
-                        <img src={tumbleweed} alt="leave fallow" style={imgStyle}/>
+                        {options.map((option, index) => {
+                            return(<SelectAction isHighlighted={option === selectedOption} image={option} altText="placeholder" key={index} onClick={() => selectOption(option)}/>)
+                        })}
                     </div>
                 </div>
                 <div className="col-9">
                     <div style={cropGridStyle}>
-                        <div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                        </div>
-                        <div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                        </div>
-                        <div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                            <div style={cropSquareStyle}>
-                                <img src={leaf} alt="crop" style={imgStyle} />
-                            </div>
-                        </div>
+                        {gameGrid.map((subArray, i) => {
+                            return (
+                                <div key={i}>
+                                    {subArray.map((cell, j) => {
+                                        return (<GameTile key={i + j} isHighlighted={false} image={leaf}/>)
+                                    })}
+                                </div>)
+                        })}
                     </div>
                 </div>
             </div>
         </div>
     )
-   
+
 }
