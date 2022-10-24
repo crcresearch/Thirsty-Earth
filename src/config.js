191b01d65dd8b41689eb84157f37440efdbb4350
Rob Wieck <rwieck@Robs-MacBook-Pro-4.local>
2022-10-24 11:05:34 -0500

    *sigh... one more time

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const PRODUCTION_ENVIRONMENT = "production"; //else development
+export const PRODUCTION_EVIRONMENT = "production"; //else development
 export const BASE_URL = "http://localhost:8000"; //need to put in port for multiplayer (none for prod)
 export const API_URL = "http://localhost:8080";
 export const DEFAULT_PORT = 8000;
+export const API_PORT = 8080;
