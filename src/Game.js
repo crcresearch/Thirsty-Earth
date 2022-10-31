54cb879f9c2a4737d3c86820eb62b85e21e3d53d
trwiley <taylor.r.wiley@gmail.com>
2022-10-31 16:54:12 -0400

    WIP: zero to one game logic

--- src/Game.js
+++ src/Game.js
@@ -3,12 +3,19 @@ import { GAME_NAME } from "./config";
 export const ThirstyEarth = {
     name: GAME_NAME,
     setup: (ctx) => { 
+        const FALLOW = 0;
+        const GROUNDWATER = 1;
+        const RAINWATER = 2;
+        const RIVERWATER = 3;
+        const defaultField = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
         const generatePlayerStats = () => {
             let stats = [];
             for(let i = 0; i < ctx.numPlayers; i++) {
                 stats.push({
                     pid: i,
-                    playerMoney: 100
+                    playerMoney: 100,
+                    playerFields: [...defaultField]
                 })
             }
             return stats;
@@ -16,11 +23,41 @@ export const ThirstyEarth = {
         let playerStats = generatePlayerStats();
         return {
             playerStats,
+            defaultField,
+            FALLOW,
+            GROUNDWATER,
+            RAINWATER,
+            RIVERWATER
+        }
+    },
+    //Reset the player's choices after they play and their new totals are calculated
+    resetPlayerBoards: (G) => {
+        for(let i = 0; i < G.playerStats.length; i++ ) {
+            G.playerStats[i].playerFields = [...G.defaultField];
         }
     },
 
     turn: {
         minMoves: 1,
         maxMoves: 1
+    },
+    phases: {
+        playerMoves: {
+            moves: {
+                  //Override the player's current selections with their new selections
+                  makeSelection: (G, ctx, newSelections) => {
+                        console.log(newSelections);
+                        G.playerStats[ctx.currentPlayer].playerFields = [...newSelections];
+                    },
+            }
+        },
+        moneyCalculation: {
+            moves: {
+                calculateNewTotals: (G, random) => {
+                    const rainfallMultiplier = random.Number();
+                    
+                }
+            }     
+        }
     }
 }
